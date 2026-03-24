0f94327272e8202f84d9d8ec54fe7de4654771be
SDN139Lamanda <radiah.tifarahs@gmail.com>
2026-03-25 01:08:29 +0800

    Update config-firebase.js

--- config-firebase.js
+++ config-firebase.js
@@ -4,15 +4,17 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
 import { getAuth, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
-
-// ⚠️ GANTI DENGAN DATA FIREBASE ANDA SENDIRI
 const firebaseConfig = {
-  apiKey: "AIzaSyDxxxx-xxxxxxxxxxxxxxx",
-  authDomain: "proyek-anda.firebaseapp.com",
-  projectId: "proyek-anda",
-  storageBucket: "proyek-anda.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:xxxxx"
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCYoUcSKj8aAZTaNgKtrn0bYHPmedVHAE4",
+  authDomain: "sdn139lamanda-a67a8.firebaseapp.com",
+  databaseURL: "https://sdn139lamanda-a67a8-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "sdn139lamanda-a67a8",
+  storageBucket: "sdn139lamanda-a67a8.firebasestorage.app",
+  messagingSenderId: "520862158793",
+  appId: "1:520862158793:web:c8cb5a4259d5e54ea06218",
+  measurementId: "G-S012NQH5LS"
 };
 
 // Initialize Firebase
