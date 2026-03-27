dd197c87931341e8658af50bf7ba436cde8bba3c
SDN139Lamanda <radiah.tifarahs@gmail.com>
2026-03-27 16:57:51 +0800

    Create auth-reset-password.js

---
+++ modules/auth-reset-password.js
@@ -0,0 +1,6 @@
+1. Navigate ke folder: modules/
+2. Klik: "Add file" → "Create new file"
+3. Nama file: auth-reset-password.js
+4. Paste kode di bawah
+5. Commit message: "Add auth-reset-password.js"
+6. Klik "Commit new file"
